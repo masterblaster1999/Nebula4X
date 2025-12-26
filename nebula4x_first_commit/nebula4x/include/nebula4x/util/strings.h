acf218eb75e6c31d50682ef67ea8d6f101035a45
masterblaster1999 <ollobrains321@gmail.com>
2025-12-26 16:01:25 +1000

    Delete nebula4x_first_commit/nebula4x directory

--- nebula4x_first_commit/nebula4x/include/nebula4x/util/strings.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace nebula4x {
-
-std::string to_lower(std::string s);
-
-} // namespace nebula4x
--- nebula4x_first_commit/nebula4x/src/ui/system_map.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "nebula4x/core/simulation.h"
-
-namespace nebula4x::ui {
-
-void draw_system_map(Simulation& sim, Id& selected_ship, double& zoom, Vec2& pan);
-
-} // namespace nebula4x::ui
